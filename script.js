11935fa5bca52a7ec61b73e83e3c0a7908a03a9f
Dmitry <dvornikov.dmitry034@gmail.com>
2025-05-24 21:51:25 +0300

    add file typescript, rewriting the script

--- script.js
+++ script.js
@@ -1,129 +1,109 @@
-class LoadPage {}
-
-class AuxiliaryFunctions {
-
-}
-
-class TaskManagement extends AuxiliaryFunctions {
-	constructor() {
-
-	}
-}
 
 
-const taskFieldPlacement = document.querySelector('.task__fieldPlacement');
-const btnAddTask = document.querySelector('.addTask__btnAdd');
+// const taskFieldPlacement = document.querySelector('.task__fieldPlacement');
+// const btnAddTask = document.querySelector('.addTask__btnAdd');
 
 // Данные для создания изначальных задач
-const data = [
-	{
-		id: 1,
-		text: 'Задача 1'
-	},
-	{
-		id: 2,
-		text: 'Задача 2'
-	}
-];
-
-// Проверка типов данных
-function exampleTypeData(inputData) {
-	let result = true;
-	inputData.forEach(data => {
-		typeof data.id !== "number" || typeof data.text !== "string" ? result = false : null;
-	});
-	return result;
-}
+// const data = [
+// 	{
+// 		id: 1,
+// 		text: 'Задача 1'
+// 	},
+// 	{
+// 		id: 2,
+// 		text: 'Задача 2'
+// 	}
+// ];
 
 // Создание начальных полей с 2 задачами
-function loadOriginTask() {
-	data.forEach(stroke => {
-		taskFieldPlacement.insertAdjacentHTML('beforeend', `
-			<div id="${stroke.id}" class="task__wrupperListTask">
-				<div id="${stroke.id}" class="task__containerText">
-					<input class="task__checkbox" type="checkbox">
-					<div id="${stroke.id}" class="task__text">${stroke.text}</div>
-				</div>
-				<div class="task__btnContainer">
-					<button id="${stroke.id}" class="task__btnEditing"></button>
-					<button id="${stroke.id}" class="task__btnSave"></button>
-					<button id="${stroke.id}" class="task__btnDelete"></button>
-				</div>
-			</div>
-		`);
-	});
-}
+// function loadOriginTask() {
+// 	data.forEach(stroke => {
+// 		taskFieldPlacement.insertAdjacentHTML('beforeend', `
+// 			<div id="${stroke.id}" class="task__wrupperListTask">
+// 				<div id="${stroke.id}" class="task__containerText">
+// 					<input class="task__checkbox" type="checkbox">
+// 					<div id="${stroke.id}" class="task__text">${stroke.text}</div>
+// 				</div>
+// 				<div class="task__btnContainer">
+// 					<button id="${stroke.id}" class="task__btnEditing"></button>
+// 					<button id="${stroke.id}" class="task__btnSave"></button>
+// 					<button id="${stroke.id}" class="task__btnDelete"></button>
+// 				</div>
+// 			</div>
+// 		`);
+// 	});
+// }
 
-// Проверка на корректные типы данных перед загрузкой страницы
-exampleTypeData(data) ? loadOriginTask() : console.log("Некорректные данные");
+
+// loadOriginTask();
 
 // Функция редактирования задач
-function updateStroke() {
-	const allBtn = document.querySelectorAll('.task__btnEditing');
-	const container = document.querySelectorAll('.task__containerText');
-	allBtn.forEach(elem => {
-		elem.addEventListener('click', (event) => {
-			let elemTarget = event.target;
-			let getIDElem = elemTarget.getAttribute('id');
-			const text = document.querySelectorAll('.task__text');
-			text.forEach(el => {
-				if (el.getAttribute('id') == getIDElem) {
-					let textElem = el.textContent;
-					let checkboxEl = el.previousElementSibling;
-					checkboxEl.checked = false; // убираем флажок с checkbox-a
-					checkboxEl.disabled = true; // делаем checkbox не активным
-					el.remove();
-					container.forEach(cont => {
-						if (cont.getAttribute('id') == getIDElem) {
-							cont.insertAdjacentHTML('beforeend', `
-								<input id="${getIDElem}" class="editingTask" value="${textElem}">
-							`)
+// function updateStroke() {
+// 	const allBtn = document.querySelectorAll('.task__btnEditing');
+// 	const container = document.querySelectorAll('.task__containerText');
+// 	allBtn.forEach(elem => {
+// 		elem.addEventListener('click', (event) => {
+// 			let elemTarget = event.target;
+// 			let getIDElem = elemTarget.getAttribute('id');
+// 			const text = document.querySelectorAll('.task__text');
+// 			text.forEach(el => {
+// 				if (el.getAttribute('id') == getIDElem) {
+// 					let textElem = el.textContent;
+// 					let checkboxEl = el.previousElementSibling;
+// 					checkboxEl.checked = false; // убираем флажок с checkbox-a
+// 					checkboxEl.disabled = true; // делаем checkbox не активным
+// 					el.remove();
+// 					container.forEach(cont => {
+// 						if (cont.getAttribute('id') == getIDElem) {
+// 							cont.insertAdjacentHTML('beforeend', `
+// 								<input id="${getIDElem}" class="editingTask" value="${textElem}">
+// 							`)
 
-						}
-					})
-					let nextButton = elemTarget.nextElementSibling;
-					nextButton.style.display = 'block';
-					elemTarget.style.display = 'none';
-				}
-			})
-		})
-	})
-}
+// 						}
+// 					})
+// 					let nextButton = elemTarget.nextElementSibling;
+// 					nextButton.style.display = 'block';
+// 					elemTarget.style.display = 'none';
+// 				}
+// 			})
+// 		})
+// 	})
+// }
 
 // Сохранение редактированных данных
-function saveStroke() {
-	const allBtnSave = document.querySelectorAll('.task__btnSave');
-	const container = document.querySelectorAll('.task__containerText');
-	allBtnSave.forEach(elem => {
-		elem.addEventListener('click', (event) => {
-			let elemTarget = event.target;
-			let getIDElem = elemTarget.getAttribute('id');
-			const input = document.querySelectorAll('.editingTask');
-			input.forEach(el => {
-				if (el.getAttribute('id') == getIDElem) {
-					let textElem = el.value;
-					if (textElem != '') {
-						let activeChecbox = el.previousElementSibling;
-						activeChecbox.disabled = false; // включаем обратно checkbox
-						el.remove();
-						container.forEach(cont => {
-							if (cont.getAttribute('id') == getIDElem) {
-								cont.insertAdjacentHTML('beforeend', `
-									<div id="${getIDElem}" class="task__text">${textElem}</div>
-								`)
-							}
-						})
-						let previousButton = elemTarget.previousElementSibling;
-						previousButton.style.display = 'block';
-						elemTarget.style.display = 'none';
-					} else {
-						el.setAttribute('placeholder', 'Заполните поле...');
-					}
-				}
-			})
-		})
-	})
-}
+// function saveStroke() {
+// 	const allBtnSave = document.querySelectorAll('.task__btnSave');
+// 	const container = document.querySelectorAll('.task__containerText');
+// 	allBtnSave.forEach(elem => {
+// 		elem.addEventListener('click', (event) => {
+// 			let elemTarget = event.target;
+// 			let getIDElem = elemTarget.getAttribute('id');
+// 			const input = document.querySelectorAll('.editingTask');
+// 			input.forEach(el => {
+// 				if (el.getAttribute('id') == getIDElem) {
+// 					let textElem = el.value;
+// 					if (textElem != '') {
+// 						let activeChecbox = el.previousElementSibling;
+// 						activeChecbox.disabled = false; // включаем обратно checkbox
+// 						el.remove();
+// 						container.forEach(cont => {
+// 							if (cont.getAttribute('id') == getIDElem) {
+// 								cont.insertAdjacentHTML('beforeend', `
+// 									<div id="${getIDElem}" class="task__text">${textElem}</div>
+// 								`)
+// 							}
+// 						})
+// 						let previousButton = elemTarget.previousElementSibling;
+// 						previousButton.style.display = 'block';
+// 						elemTarget.style.display = 'none';
+// 					} else {
+// 						el.setAttribute('placeholder', 'Заполните поле...');
+// 					}
+// 				}
+// 			})
+// 		})
+// 	})
+// }
 
 // Удаление задачи
 function deleteStroke() {
